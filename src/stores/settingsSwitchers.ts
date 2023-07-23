0b37117fb14c448b0e455aaa6016538d9a50e3cf
Juris-Berkulis <krztfjrbhm1@gmail.com>
2023-07-24 00:28:08 +0300

    1. Fix a bug where the new minefield settings added to the application did not take the default value if the user has previously saved other settings.

--- src/stores/settingsSwitchers.ts
+++ src/stores/settingsSwitchers.ts
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { reactive, toRef, type Ref } from 'vue';
+import { reactive, ref, toRef, type Ref } from 'vue';
 
 interface SettingsSwitchers {
     isVibrationForFlags: boolean,
@@ -13,22 +13,22 @@ export const useSettingsSwitchersStore = defineStore('settingsSwitchers', () => 
     };
 
     //* Весь объект настроек:
-    const settingsSwitchers: SettingsSwitchers = reactive(JSON.parse(localStorage.getItem('settingsSwitchers') || JSON.stringify(settingsSwitchersDefault)));
+    const settingsSwitchers: SettingsSwitchers = reactive(JSON.parse(localStorage.getItem('settingsSwitchers') || '{}'));
 
     //* Вибрация при установке и удалении флагов:
-    const isVibrationForFlags: Ref<boolean> = toRef(settingsSwitchers, 'isVibrationForFlags');
+    const isVibrationForFlags: Ref<boolean> = settingsSwitchers.isVibrationForFlags !== undefined ? toRef(settingsSwitchers, 'isVibrationForFlags') : ref(settingsSwitchersDefault.isVibrationForFlags);
 
     const setIsVibrateForFlags = (): void => {
         isVibrationForFlags.value = !isVibrationForFlags.value;
-        localStorage.setItem('settingsSwitchers', JSON.stringify(settingsSwitchers));
+        localStorage.setItem('settingsSwitchers', JSON.stringify({...settingsSwitchers, isVibrationForFlags: isVibrationForFlags.value}));
     };
 
     //* Флаг защищает ячейку от случайного нажатия:
-    const isFlagProtectsCellFromAccidentalClick: Ref<boolean> = toRef(settingsSwitchers, 'isFlagProtectsCellFromAccidentalClick');
+    const isFlagProtectsCellFromAccidentalClick: Ref<boolean> = settingsSwitchers.isFlagProtectsCellFromAccidentalClick !== undefined ? toRef(settingsSwitchers, 'isFlagProtectsCellFromAccidentalClick') : ref(settingsSwitchersDefault.isFlagProtectsCellFromAccidentalClick);
 
     const setIsFlagProtectsCellFromAccidentalClick = (): void => {
         isFlagProtectsCellFromAccidentalClick.value = !isFlagProtectsCellFromAccidentalClick.value;
-        localStorage.setItem('settingsSwitchers', JSON.stringify(settingsSwitchers));
+        localStorage.setItem('settingsSwitchers', JSON.stringify({...settingsSwitchers, isFlagProtectsCellFromAccidentalClick: isFlagProtectsCellFromAccidentalClick.value}));
     };
 
     return {
