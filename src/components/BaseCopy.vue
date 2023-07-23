1afe3105597c293832e670f36611162ab95d19b7
Juris-Berkulis <krztfjrbhm1@gmail.com>
2023-07-24 02:19:30 +0300

    1. Add a button to copy the link to the application; 2. Increase the button to go to the page with a minefield.

---
+++ src/components/BaseCopy.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
+
+const copySuccess: Ref<boolean> = ref(false);
+const copyError: Ref<boolean> = ref(false);
+
+const navigatorClipboard = navigator.clipboard;
+
+const copyLink = (): void => {
+    copySuccess.value = false;
+    copyError.value = false;
+
+    if (navigatorClipboard) {
+        navigator.clipboard.writeText(location.hostname)
+            .then(() => {
+                copySuccess.value = true;
+            })
+            .catch(err => {
+                copyError.value = true;
+            });
+    }
+}
+</script>
+
+<template>
+<div class="copyWrapper" v-if="navigatorClipboard">
+    <button class="copyBtn" @click="copyLink">Поделиться ссылкой на приложение</button>
+    <div class="copyTextContainer">
+        <p class="copyText copySuccess" v-if="copySuccess">Ссылка скопирована в буфер обмена</p>
+        <p class="copyText copyError" v-else-if="copyError">Ошибка копирования ссылки</p>
+    </div>
+</div>
+</template>
+
+<style scoped>
+.copyWrapper {
+    width: 100%;
+    margin-bottom: calc(15px + 1em);
+}
+
+.copyBtn {
+    width: 100%;
+    padding: 5px;
+    border-radius: 5px;
+    border: 3px solid currentColor;
+    background-color: transparent;
+    font-size: 1em;
+    font-weight: 700;
+    line-height: 1.15;
+    color: #6497d4;
+    transition: all 0.4s ease-in-out 0s;
+}
+
+.copyBtn:hover {
+    color: #3d72b3;
+}
+
+.copyTextContainer {
+    position: relative;
+    height: 0;
+    width: 100%;
+    margin-top: 5px;
+}
+
+.copyText {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    font-size: 0.8em;
+    font-weight: 700;
+    line-height: 1.15;
+    text-align: center;
+}
+
+.copySuccess {
+    color: #008000;
+}
+
+.copyError {
+    color: #ff0000;
+}
+</style>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
+import BaseCopy from '@/components/BaseCopy.vue';
 import TheFieldSize from '@/components/TheFieldSize.vue';
 import TheSettingsSwitchers from '@/components/TheSettingsSwitchers.vue';
-
-
 </script>
 
 <template>
     <div class="setting">
         <RouterLink class="link" id="homeView" to="/">Вернуться в поле</RouterLink>
+        <BaseCopy />
         <TheFieldSize />
         <TheSettingsSwitchers />
     </div>
@@ -40,7 +40,7 @@ import TheSettingsSwitchers from '@/components/TheSettingsSwitchers.vue';
     display: block;
     width: 100%;
     margin-bottom: 20px;
-    padding: 5px 15px;
+    padding: 8px 15px;
     text-align: center;
     background-color: #449944;
     font-size: 1em;
